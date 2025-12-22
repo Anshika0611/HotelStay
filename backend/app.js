29c1c273507f6ecf10bd3f0a49528832afac23f4
Anshika Awasthi <114733427+Anshika0611@users.noreply.github.com>
2025-12-22 16:49:16 +0530

    small change

--- backend/app.js
+++ backend/app.js
@@ -126,7 +126,7 @@ app.use("/", userRouter);
 
 // lets create a generic route for when none of the above path match
 app.use((req, res, next) => {
-  req.flash("error","The Page You Requested DNE")
+  // req.flash("error","The Page You Requested DNE")
   setTimeout(() => {
     res.redirect('/listing')
   }, 1000);
